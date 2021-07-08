0a8a5e931b188966e07c46c2c8fede2b818a0d6e
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2021-07-08 20:23:59 +0200

    BUILD : Configuration eslint

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
+app.use(require('./routes'))
 
 app.listen(3000, function () {
-    console.log('Server running on localhost:3000')
+  console.log('Server running on localhost:3000')
 })
