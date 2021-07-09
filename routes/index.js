2b7b44684157fb9f96e8776601f1768674ff4672
Derousseaux Nathanaël <nathanael.derousseaux@outlook.fr>
2021-07-09 11:37:10 +0200

    FEAT: Retour d'une phrase aléatoire

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
+const Phrase = require('../utils/fonctions.js').phrase
 const router = require('express').Router()
 
 router.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send(Phrase().corps)
 })
 
 module.exports = router
