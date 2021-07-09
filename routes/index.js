50f160f8c9b9539308e3368972175ddef5082a90
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2021-07-09 12:26:08 +0200

    FEAT: Séléction d'une taille minimale et/ou maximale

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,37 @@
-const Phrase = require('../utils/fonctions.js').phrase
+const Phrase = require('../utils/fonctions.js')
 const router = require('express').Router()
 
 router.get('/', function (req, res) {
-  res.send(Phrase().corps)
+  let min_length = parseInt(req.query.min_length) || 0
+  let max_length = parseInt(req.query.max_length) || 999999999
+  let phrase = ''
+  let i = 0
+
+  // Testing the consistency of query param
+
+  if (min_length > max_length){
+    return res.status(400).json(
+      { error: "min_length cannot be upper than max_length"}
+    )
+  }
+
+  //Generating sentence
+  do{
+    phrase = Phrase().corps
+    i += 1
+
+    // If no sentence was found
+    if (i > 10000){
+      return res.status(400).json(
+        { error: "no sentence was found"}
+      )
+    }
+  } while(
+    min_length>phrase.length ||
+    max_length<phrase.length
+  )
+
+  res.send(phrase)
 })
 
 module.exports = router
