b3bfb4fa4e61a94dafafeeae1ef3e0d63f785696
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2021-07-09 15:32:20 +0200

    REFRACTOR: Formatage du code

--- routes/index.js
+++ routes/index.js
@@ -2,33 +2,33 @@ const Phrase = require('../utils/fonctions.js')
 const router = require('express').Router()
 
 router.get('/', function (req, res) {
-  let min_length = parseInt(req.query.min_length) || 0
-  let max_length = parseInt(req.query.max_length) || 999999999
+  const minLength = parseInt(req.query.minLength) || 0
+  const maxLength = parseInt(req.query.maxLength) || 999999999
   let phrase = ''
   let i = 0
 
   // Testing the consistency of query param
 
-  if (min_length > max_length){
+  if (minLength > maxLength) {
     return res.status(400).json(
-      { error: "min_length cannot be upper than max_length"}
+      { error: 'minLength cannot be upper than maxLength' }
     )
   }
 
-  //Generating sentence
-  do{
+  // Generating sentence
+  do {
     phrase = Phrase().corps
     i += 1
 
     // If no sentence was found
-    if (i > 10000){
+    if (i > 10000) {
       return res.status(400).json(
-        { error: "no sentence was found"}
+        { error: 'no sentence was found' }
       )
     }
-  } while(
-    min_length>phrase.length ||
-    max_length<phrase.length
+  } while (
+    minLength > phrase.length ||
+    maxLength < phrase.length
   )
 
   res.send(phrase)
